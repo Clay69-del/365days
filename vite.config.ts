f0cf80ce73af4144615481986543411fb7ec3ce2
Your Name <your.email@example.com>
2026-01-13 10:06:27 +0545

    Update repository name to 365days

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/FUCK/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/365days/' : '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
