0e9abfb6252bf5fdc5bee9641fbd452bcfe31935
Your Name <your.email@example.com>
2026-01-13 10:01:51 +0545

    Add GitHub Actions workflow and update config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/FUCK/' : '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+  },
 })
